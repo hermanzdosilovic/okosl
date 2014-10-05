a4b9f01bd6f4180ba33e78f386a56a595666b0d4
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2014-10-05 18:32:19 +0200

    Finished week01

---
+++ homeworks/homework01/hw01-0036480275.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /tmp
+mkdir OKOSL
+mkdir OKOSL/{sijecanj,veljaca,ozujak,travanj,svibanj,lipanj,srpanj,kolovoz,rujan,listopad,studeni,prosinac}
+mkdir OKOSL/listopad/.Prvo\ predavanje
+cd
+ls -Rral --sort=time /tmp/OKOSL
