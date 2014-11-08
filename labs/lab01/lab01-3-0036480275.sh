32e2be724613a86292802df3cb20e1861faef82d
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2014-11-08 12:26:41 +0100

    Small mistake.

--- labs/lab01/lab01-3-0036480275.sh
+++ labs/lab01/lab01-3-0036480275.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo find /var/log -name '*.log' -o -name '*.dat'
+sudo find /var/log -name '*.log' -o -name '*.dat' >> /tmp/tmp_ispis
 calendar -f /usr/share/calendar/calendar.lotr -A 365 | egrep -i -A 1 -B 1 "gandalf"
 calendar -f /usr/share/calendar/calendar.lotr -A 365 | egrep -i "gandalf$" >> /tmp/tmp_ispis
